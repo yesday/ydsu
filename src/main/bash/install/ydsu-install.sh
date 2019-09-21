ae41137a275bf2ce5550c75bece68edf82772573
YesDay <yesdayuk@gmail.com>
2019-09-21 17:51:43 +0100

    The new release includes tremendous improvements: refined internal util module, implementation of ydsuPull and ydsuUninstall scripts, DockerClient, etc.

--- src/main/bash/install/ydsu-install.sh
+++ src/main/bash/install/ydsu-install.sh
@@ -16,18 +16,18 @@
 #
 
 if [ -z "$YDSU_HOME" ]; then
-    YDSU_HOME="$HOME/appdata/ydsu"
+  YDSU_HOME="$HOME/appdata/ydsu"
 fi
-YDSU_CACHE="${YDSU_HOME}/cache"
-YDSU_MODULE="${YDSU_HOME}/src/main/groovy/ydsu/module"
-
-# Local variables
-ydsu_repo=https://github.com/yesday/ydsu.git
+YDSU_SOURCE="${YDSU_HOME}/source/yesday/ydsu"
+YDSU_MODULE="${YDSU_SOURCE}/src/main/groovy/ydsu/module"
+if [ -z "$YDSU_REPO" ]; then
+  YDSU_REPO=https://github.com/yesday/ydsu.git
+fi
 
 echo ''
 echo '              _'
 echo '             | |'
-echo '  _   _    __| |  ___   _   _' 
+echo '  _   _    __| |  ___   _   _'
 echo ' | | | |  / _` | / __| | | | |'
 echo ' | |_| | | (_| | \__ \ | |_| |'
 echo '  \__, |  \__,_| |___/  \__,_|'
@@ -39,80 +39,82 @@ echo '                                                                 Now attem
 echo ''
 echo ''
 
-echo "Looking for a previous installation of YDSU..."
-if [ -d "$YDSU_HOME" ]; then
-	echo "YDSU found."
-	echo ""
-	echo "======================================================================================================"
-	echo " You already have YDSU installed."
-	echo " YDSU was found at:"
-	echo ""
-	echo "    ${YDSU_HOME}"
-	echo ""
-	echo " Please consider running the following if you need to upgrade."
-	echo ""
-	echo "    $ ydsuPull"
-	echo ""
-	echo " Alternatively, to reset the existing installation perform the following steps."
-	echo ""
-	echo "    $ ydsuUninstall"
-	echo "    Manually delete ${YDSU_HOME}"
-	echo "    Run this script again"
-	echo ""
-	echo "======================================================================================================"
-	echo ""
-	exit 0
+echo "Looking for a previous installation of ydsu..."
+if [ -d "$YDSU_SOURCE" ]; then
+  echo "ydsu found."
+  echo ""
+  echo "======================================================================================================"
+  echo " You already have ydsu installed."
+  echo " ydsu was found at:"
+  echo ""
+  echo "    ${YDSU_SOURCE}"
+  echo ""
+  echo " Please consider running the following if you need to upgrade."
+  echo ""
+  echo "    $ ydsuPull"
+  echo ""
+  echo " Alternatively, to reset the existing installation perform the following steps."
+  echo ""
+  echo "    $ ydsuUninstall"
+  echo "    Run this script again"
+  echo ""
+  echo "======================================================================================================"
+  echo ""
+  exit 0
 fi
 
 echo "Looking for git..."
 if [ -z $(which git) ]; then
-	echo "Not found."
-	echo "======================================================================================================"
-	echo " Please install git on your system using your favourite package manager."
-	echo ""
-	echo " Restart after installing git."
-	echo "======================================================================================================"
-	echo ""
-	exit 1
+  echo "Not found."
+  echo "======================================================================================================"
+  echo " Please install git on your system using your favourite package manager."
+  echo ""
+  echo " Restart after installing git."
+  echo "======================================================================================================"
+  echo ""
+  exit 1
 fi
 
 echo "Looking for groovy..."
 if [ -z $(which groovy) ]; then
-	echo "Not found."
-	echo "======================================================================================================"
-	echo " Please install groovy on your system using your favourite package manager."
-	echo ""
-	echo " Restart after installing groovy."
-	echo "======================================================================================================"
-	echo ""
-	exit 1
+  echo "Not found."
+  echo "======================================================================================================"
+  echo " Please install groovy on your system using your favourite package manager."
+  echo ""
+  echo " Restart after installing groovy."
+  echo "======================================================================================================"
+  echo ""
+  exit 1
 fi
 
 echo "Looking for ansible..."
 if [ -z $(which ansible) ]; then
-	echo "Not found."
-	echo "======================================================================================================"
-	echo " Please install ansible on your system using your favourite package manager."
-	echo ""
-	echo " Restart after installing ansible."
-	echo "======================================================================================================"
-	echo ""
-	exit 1
+  echo "Not found."
+  echo "======================================================================================================"
+  echo " Please install ansible on your system using your favourite package manager."
+  echo ""
+  echo " Restart after installing ansible."
+  echo "======================================================================================================"
+  echo ""
+  exit 1
 fi
 
-echo "Installing YDSU with default distribution..."
+echo "Installing ydsu..."
 
 # Create directory structure
 
 echo "Create distribution directories..."
 # The following `cd; cd -` prevents the error caused by (optionally) re-setting the ydsu directory:
 # shell-init: error retrieving current directory: getcwd: cannot access parent directories: No such file or directory
-cd; cd -
-mkdir -p "${YDSU_CACHE}/yesday"
-git clone ${ydsu_repo} "${YDSU_CACHE}/yesday/ydsu"
-cp -R "${YDSU_CACHE}/yesday/ydsu/src" "${YDSU_HOME}"
-cp -R "${YDSU_CACHE}/yesday/ydsu/src/main/resources/distribution" "$YDSU_HOME"
-cp -R "${YDSU_CACHE}/yesday/ydsu/src/main/resources/version.txt" "$YDSU_HOME"
+cd
+cd -
+mkdir -p "${YDSU_HOME}/source/yesday/"
+if [[ $YDSU_REPO == /* ]]; then
+  # if it's a local directory then just copy it's contents
+  cp -R ${YDSU_REPO} "${YDSU_SOURCE}"
+else
+  git clone ${YDSU_REPO} "${YDSU_SOURCE}"
+fi
 
 PATH="$PATH:\
 ${YDSU_MODULE}/manage:\
